82bb88ac3696cd9f7c454dd66ad75aaca162927d
TreadUp User <user@treadup.com>
2026-06-17 20:20:40 +0100

    chore: change ports - backend 4000, frontend 4001

--- Occasion&garantie/frontend/vite.config.js
+++ Occasion&garantie/frontend/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5009,
+    port: 4001,
     proxy: {
-      '/api': 'http://localhost:5002',
-      '/uploads': 'http://localhost:5002'
+      '/api': 'http://localhost:4000',
+      '/uploads': 'http://localhost:4000'
     }
   }
 });
