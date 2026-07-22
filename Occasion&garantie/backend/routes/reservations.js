8af2da131c120e35b150944ecac6ecef3fc376cf
abdel <abdel@example.com>
2026-07-22 21:38:53 +0100

    Fix missing express require and Router() definition in reservations.js

--- Occasion&garantie/backend/routes/reservations.js
+++ Occasion&garantie/backend/routes/reservations.js
@@ -1,4 +1,6 @@
+const express = require('express');
 const crypto = require('crypto');
+const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
