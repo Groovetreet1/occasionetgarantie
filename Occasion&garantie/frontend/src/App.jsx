23c4d82d06bec7c6bbd1fbebed9af8664a9a3fdb
abdel <abdel@example.com>
2026-07-30 20:48:28 +0100

    Fix 404 - add routes for /admin/products/new and /admin/products/edit/:id

--- Occasion&garantie/frontend/src/App.jsx
+++ Occasion&garantie/frontend/src/App.jsx
@@ -30,6 +30,7 @@ import AdminVendorLogs from './pages/AdminVendorLogs';
 import AdminManagedVendors from './pages/AdminManagedVendors';
 import AdminPendingProducts from './pages/AdminPendingProducts';
 import AdminStoreProducts from './pages/AdminStoreProducts';
+import AdminProductForm from './pages/AdminProductForm';
 import SellPage from './pages/SellPage';
 import SellerDashboard from './pages/SellerDashboard';
 import SellerProductForm from './pages/SellerProductForm';
@@ -96,6 +97,8 @@ export default function App() {
           <Route path="/admin/managed-vendors" element={<AnimatedPage><AdminRoute><AdminManagedVendors /></AdminRoute></AnimatedPage>} />
           <Route path="/admin/products/pending" element={<AnimatedPage><AdminRoute><AdminPendingProducts /></AdminRoute></AnimatedPage>} />
           <Route path="/admin/store-products" element={<AnimatedPage><AdminRoute><AdminStoreProducts /></AdminRoute></AnimatedPage>} />
+          <Route path="/admin/products/new" element={<AnimatedPage><AdminRoute><AdminProductForm /></AdminRoute></AnimatedPage>} />
+          <Route path="/admin/products/edit/:id" element={<AnimatedPage><AdminRoute><AdminProductForm /></AdminRoute></AnimatedPage>} />
           <Route path="/admin/products" element={<AnimatedPage><AdminRoute><AdminDashboard /></AdminRoute></AnimatedPage>} />
           <Route path="/vendre" element={<AnimatedPage><SellPage /></AnimatedPage>} />
           <Route path="/reprise" element={<AnimatedPage><RepriseForm /></AnimatedPage>} />
